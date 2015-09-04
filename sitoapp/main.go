5c95895e72f29960891a80ece7743e30824dd644
Pouya <pouyajoon@gmail.com>
2015-09-05 00:40:35 +0200

    introdcute ws

--- sitoapp/main.go
+++ sitoapp/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/websocket"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	log "sito/Godeps/_workspace/src/github.com/Sirupsen/logrus"
-	"sito/Godeps/_workspace/src/github.com/codegangsta/negroni"
 )
 
 // type Page struct {
@@ -42,7 +44,65 @@ func publicHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleWebsocket(w http.ResponseWriter, r *http.Request) {}
+// connection is an middleman between the websocket connection and the hub.
+type connection struct {
+	// The websocket connection.
+	ws *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.WithField("err", err).Println("Upgrading to websockets")
+		http.Error(w, "Error Upgrading to websockets", 400)
+		return
+	}
+
+	// id := rr.register(ws)
+
+	for {
+		mt, data, err := ws.ReadMessage()
+		ctx := log.Fields{"mt": mt, "data": data, "err": err}
+		if err != nil {
+			if err == io.EOF {
+				log.WithFields(ctx).Info("Websocket closed!")
+			} else {
+				log.WithFields(ctx).Error("Error reading websocket message")
+			}
+			break
+		}
+		switch mt {
+		case websocket.TextMessage:
+			msg, err := validateMessage(data)
+			if err != nil {
+				ctx["msg"] = msg
+				ctx["err"] = err
+				log.WithFields(ctx).Error("Invalid Message")
+				break
+			}
+			rw.publish(data)
+		default:
+			log.WithFields(ctx).Warning("Unknown Message!")
+		}
+	}
+
+	// rr.deRegister(id)
+
+	ws.WriteMessage(websocket.CloseMessage, []byte{})
+}
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// title := r.URL.Path[len("/view/"):]
--- sitoapp/public/js/main.js
+++ sitoapp/public/js/main.js
@@ -1,4 +1,11 @@
 (function() {
   'use strict';
+  /*global WebSocket*/
+
+  var ws = new WebSocket('ws://localhost:8081', "ProtocolOne");
+  ws.onopen = function() {
+    console.log('ws open');
+  };
+
   console.log('hello');
 }());
